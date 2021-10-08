d66ba2a058744c4c64f1c86b4ff73b0062571db1
SHEKHAR J. DHRUV <91380977+shekhar-dhruv-21@users.noreply.github.com>
2021-10-09 00:34:36 +0530

    Add files via upload

---
+++ os_module.js
@@ -0,0 +1,7 @@
+const os = require("os");
+
+console.log("OS CPU architecture : " + os.arch);
+console.log("Free memory of the system : " + os.freemem/(1024*1024*1024) + " GB");
+console.log("Total memory of the system : " + os.totalmem/(1024*1024*1024) + " GB");
+console.log("OS Platform : " + os.platform);
+console.log("Information about the current user : " + os.userInfo().username);
